361b4560759a1dd272c60680588296c580f22c7f
MichaelMisquitta <michaelmisquitta@gmail.com>
2023-12-15 23:56:59 -0600

    added velocity and gravity

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ gamescreen = pygame.display.set_mode((400,400));
 clock = pygame.time.Clock()
 running = True
 playerPos = pygame.Vector2(gamescreen.get_width()/2, gamescreen.get_height()/2)
+playerVelocity = [0,0]
+gravity = 100
 dt = 0
+ground = gamescreen.get_height()*3/4
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -16,12 +20,29 @@ while running:
     if keys[pygame.K_UP]:
         playerPos.y -= 300*dt
     if keys[pygame.K_DOWN]:
-        playerPos.y += 300*dt
+        if playerPos.y+20 < ground:
+            playerPos.y += 300*dt
+        else:
+            playerPos.y = ground - 20
     if keys[pygame.K_LEFT]:
         playerPos.x -= 300*dt
     if keys[pygame.K_RIGHT]:
         playerPos.x += 300*dt
+    if keys[pygame.K_SPACE]:
+        playerVelocity[1] = -100
+
+    #implement gravity and resistance here
+        
+    #gravity
+    playerVelocity[1] += gravity*dt
+
+    playerPos.y = playerVelocity[1]*dt + playerPos.y
+    if playerPos.y+20 > ground:
+        playerPos.y = ground - 20
+        playerVelocity[1] = 0
+
     
+
     pygame.display.flip()
 
     dt = clock.tick(60)/1000
